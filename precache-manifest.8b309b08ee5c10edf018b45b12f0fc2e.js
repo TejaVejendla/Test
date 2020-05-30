d4bc7d51b7fc949425439f9bbba72a9aa2004d6d
TEJA SPEED <TEJA_1122@hotmail.com>
2020-05-30 17:09:14 +1000

    Build

---
+++ precache-manifest.8b309b08ee5c10edf018b45b12f0fc2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e0f580c944ec650381427361f20aad9",
+    "url": "/React.Todo/index.html"
+  },
+  {
+    "revision": "03a6de4c011b85326b9e",
+    "url": "/React.Todo/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "52a37682217bc82c6124",
+    "url": "/React.Todo/static/css/main.281a20d3.chunk.css"
+  },
+  {
+    "revision": "03a6de4c011b85326b9e",
+    "url": "/React.Todo/static/js/2.02d9bad8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React.Todo/static/js/2.02d9bad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a37682217bc82c6124",
+    "url": "/React.Todo/static/js/main.0fc98bde.chunk.js"
+  },
+  {
+    "revision": "09adbf802f517df08c83",
+    "url": "/React.Todo/static/js/runtime-main.254736e4.js"
+  }
+]);
