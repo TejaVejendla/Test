461121ad2aedbe4b41c70d3bd162a8425bdf7a73
TEJA SPEED <TEJA_1122@hotmail.com>
2020-05-30 10:58:10 +0000

    Release Version 2020.05.30.2.346

---
+++ precache-manifest.e5f50dec27d45dbe12edbf6f3674f7ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d27304269bb4ce71054e172430ad732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03a6de4c011b85326b9e",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b925acd6aa122b07ba52",
+    "url": "/static/css/main.18cb7f7a.chunk.css"
+  },
+  {
+    "revision": "03a6de4c011b85326b9e",
+    "url": "/static/js/2.02d9bad8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02d9bad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b925acd6aa122b07ba52",
+    "url": "/static/js/main.8ca186a1.chunk.js"
+  },
+  {
+    "revision": "6a59e1b3cc1c6de8a868",
+    "url": "/static/js/runtime-main.4c5084bf.js"
+  }
+]);
